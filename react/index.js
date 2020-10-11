68fec4c296e2e85415f404c8dbfd8f12dae601ae
lehanshu <lehanshu@gmail.com>
2020-10-12 01:03:17 +0530

    React Score App v1

--- react/index.js
+++ react/index.js
@@ -2,45 +2,190 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var LikeButton = function (_React$Component) {
-  _inherits(LikeButton, _React$Component);
+var playerID = 0;
 
-  function LikeButton(props) {
-    _classCallCheck(this, LikeButton);
+var AddPlayer = function (_React$Component) {
+    _inherits(AddPlayer, _React$Component);
 
-    var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
+    function AddPlayer(props) {
+        _classCallCheck(this, AddPlayer);
 
-    _this.state = { liked: false };
-    return _this;
-  }
+        var _this = _possibleConstructorReturn(this, (AddPlayer.__proto__ || Object.getPrototypeOf(AddPlayer)).call(this, props));
 
-  _createClass(LikeButton, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
+        _this.onPlayerTextChange = function (e) {
+            _this.setState({ playerName: e.target.value });
+        };
 
-      if (this.state.liked) {
-        return 'You liked this OKay?.';
-      }
+        _this.sendData = function () {
+            _this.props.parentCallback(_this.state.playerName);
+            _this.setState({
+                playerName: ''
+            });
+        };
 
-      return React.createElement(
-        'button',
-        { onClick: function onClick() {
-            return _this2.setState({ liked: true });
-          } },
-        'Like'
-      );
+        _this.state = { playerName: '' };
+        return _this;
     }
-  }]);
 
-  return LikeButton;
+    _createClass(AddPlayer, [{
+        key: 'render',
+        value: function render() {
+            var _this2 = this;
+
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(
+                    'p',
+                    { className: 'navLink' },
+                    ' Adding player here : ',
+                    this.state.playerName
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'row' },
+                    React.createElement(
+                        'div',
+                        { className: 'row' },
+                        React.createElement(
+                            'div',
+                            { className: 'input-field col s3' },
+                            React.createElement(
+                                'i',
+                                { className: 'material-icons prefix' },
+                                'account_circle'
+                            ),
+                            React.createElement('input', { id: 'icon_prefix', type: 'text', className: 'validate', value: this.state.playerName,
+                                onChange: function onChange(e) {
+                                    return _this2.onPlayerTextChange(e);
+                                } }),
+                            React.createElement(
+                                'label',
+                                { htmlFor: 'icon_prefix' },
+                                'Player Name'
+                            ),
+                            React.createElement(
+                                'button',
+                                { className: 'btn waves-effect waves-light',
+                                    onClick: function onClick() {
+                                        return _this2.sendData();
+                                    } },
+                                'Submit',
+                                React.createElement(
+                                    'i',
+                                    { className: 'material-icons right' },
+                                    'send'
+                                )
+                            )
+                        )
+                    )
+                )
+            );
+        }
+    }]);
+
+    return AddPlayer;
 }(React.Component);
 
-var domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(React.createElement(LikeButton, null), domContainer);
+var AppContainer = function (_React$Component2) {
+    _inherits(AppContainer, _React$Component2);
+
+    function AppContainer(props) {
+        _classCallCheck(this, AppContainer);
+
+        var _this3 = _possibleConstructorReturn(this, (AppContainer.__proto__ || Object.getPrototypeOf(AppContainer)).call(this, props));
+
+        _this3.callbackFunction = function (childData) {
+            _this3.setState(function (prevState) {
+                return {
+                    playerList: [].concat(_toConsumableArray(prevState.playerList), [{
+                        id: playerID++,
+                        Name: childData,
+                        score: []
+                    }])
+                };
+            });
+        };
+
+        _this3.state = {
+            liked: false,
+            playerList: []
+        };
+        return _this3;
+    }
+
+    _createClass(AppContainer, [{
+        key: 'render',
+        value: function render() {
+            var _this4 = this;
+
+            if (this.state.liked) {
+                return 'You liked this OKay?.';
+            }
+
+            return React.createElement(
+                'div',
+                null,
+                React.createElement(AddPlayer, { parentCallback: this.callbackFunction }),
+                React.createElement(
+                    'p',
+                    null,
+                    'PlayerNameLength: ',
+                    this.state.playerList.length
+                ),
+                React.createElement(
+                    'ul',
+                    { className: 'collection' },
+                    this.state.playerList.map(function (player) {
+                        return React.createElement(
+                            'li',
+                            { key: player.id, className: 'collection-item' },
+                            player.Name,
+                            '+',
+                            player.id
+                        );
+                    })
+                ),
+                React.createElement(
+                    'table',
+                    { className: 'striped' },
+                    React.createElement(
+                        'thead',
+                        null,
+                        React.createElement(
+                            'tr',
+                            null,
+                            this.state.playerList.map(function (player) {
+                                return React.createElement(
+                                    'th',
+                                    { key: player.id },
+                                    player.Name
+                                );
+                            })
+                        )
+                    )
+                ),
+                React.createElement(
+                    'button',
+                    { onClick: function onClick() {
+                            return _this4.setState({ liked: true });
+                        }, className: 'buttonClass' },
+                    'Like'
+                )
+            );
+        }
+    }]);
+
+    return AppContainer;
+}(React.Component);
+
+var domContainer = document.querySelector('#appContainer');
+ReactDOM.render(React.createElement(AppContainer, null), domContainer);
--- react/src/index.js
+++ react/src/index.js
@@ -1,10 +1,73 @@
 'use strict';
 
-class LikeButton extends React.Component {
+
+let playerID = 0;
+
+class AddPlayer extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {playerName:'',}
+    }
+
+onPlayerTextChange = (e) =>{
+    this.setState({playerName:e.target.value});
+}
+sendData = () => {
+    this.props.parentCallback(this.state.playerName);
+    this.setState({
+        playerName:'',
+    })
+}
+    render() {
+    return (
+        <div>
+            <p className="navLink"> Adding player here : {this.state.playerName}</p>
+            <div className="row">
+                
+                <div className="row">
+                    <div className="input-field col s3">
+                        <i className="material-icons prefix">account_circle</i>
+                        <input id="icon_prefix" type="text" className="validate" value={this.state.playerName} 
+                        onChange={e => this.onPlayerTextChange(e) } />
+                        <label htmlFor="icon_prefix">Player Name</label>
+                        <button className="btn waves-effect waves-light" 
+                        onClick={() => this.sendData()}>Submit
+                            <i className="material-icons right">send</i>
+                        </button>
+                        
+                    </div>
+                    
+                </div>
+                
+            </div>
+        
+      </div>
+    );
+  }
+}
+
+
+class AppContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false };
+    this.state = { 
+        liked: false,
+        playerList : [],
+    };
   }
+
+callbackFunction =(childData) =>{
+    this.setState(
+        prevState =>({
+            playerList:[...prevState.playerList,
+                                    {
+                                        id : playerID++,
+                                        Name:childData,
+                                        score:[]
+                                    }],
+        })
+    );
+}
 
   render() {
     if (this.state.liked) {
@@ -12,12 +75,43 @@ class LikeButton extends React.Component {
     }
 
     return (
-      <button onClick={() => this.setState({ liked: true }) }>
+        <div>
+            
+        <AddPlayer parentCallback = {this.callbackFunction} />
+        <p>PlayerNameLength: {this.state.playerList.length}</p>
+            <ul className="collection">
+                {this.state.playerList.map(
+                    player =>(
+                        <li key={player.id} className="collection-item">{player.Name}+{player.id}</li>
+                    )                    
+                )}
+                
+                </ul>
+
+                <table className="striped">
+                    <thead>
+                        <tr>
+                        {this.state.playerList.map(
+                            player=>(
+                                <th key={player.id}>{player.Name}</th>
+                            )
+                        )}  
+                        </tr>  
+                    </thead>
+                   
+                </table>
+
+
+      <button onClick={() => this.setState({ liked: true }) } className="buttonClass" >
         Like
       </button>
+      </div>
     );
   }
 }
 
-let domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(<LikeButton />, domContainer);
+let domContainer = document.querySelector('#appContainer');
+ReactDOM.render(<AppContainer />, domContainer);
+
+
+
